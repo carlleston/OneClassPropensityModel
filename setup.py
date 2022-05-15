da877581d5e904a06cbff00b64715f7fd0b8c9f2
Anthony Carlleston de Lima <52056552+carlleston@users.noreply.github.com>
2022-05-15 17:18:54 -0300

    full project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='tcc using one-class for propension modelling',
+    author='anthonycarllestonlima',
+    license='',
+)
